dba4a55bc14eab533d86c463927f03e251ea2e0a
NSJessen <niklas.spanggaard.jessen@gmail.com>
2026-01-30 23:29:13 +0100

    Update Launcher.java

--- src/main/java/tidsrejse/agenturet/tidsrejseagenturet/Launcher.java
+++ src/main/java/tidsrejse/agenturet/tidsrejseagenturet/Launcher.java
@@ -1,13 +1,19 @@
 package tidsrejse.agenturet.tidsrejseagenturet;
 
+import config.DatabaseConfig;
 import javafx.application.Application;
 import systems.TimeTravelSystem;
 
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 public class Launcher {
     public static void main(String[] args) throws SQLException {
         TimeTravelSystem tts = new TimeTravelSystem();
+        DatabaseConfig dc = new DatabaseConfig();
+
+        dc.dbInit();
+
 
         Application.launch(HelloApplication.class, args);
     }
